6991c98f52658f35994e6d68286fca287d4cade5
cordell <cordellsmail@gmail.com>
2012-11-07 17:49:44 -0600

    first commit

---
+++ slim.mk
@@ -0,0 +1,29 @@
+$(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := d2spr
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/cdma.mk)
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device settings
+$(call inherit-product, vendor/slim/config/common_sgs.mk)
+
+#copy 00check
+PRODUCT_COPY_FILES += \
+	vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
+
+#bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2spr TARGET_DEVICE=d2spr BUILD_FINGERPRINT="d2spr-user 4.0.4 IMM76D L710VPALG2 release-keys" PRIVATE_BUILD_DESC="samsung/d2spr/d2spr:4.0.4/IMM76D/L710VPALG2:user/release-keys"
+
+PRODUCT_NAME := slim_d2spr
+PRODUCT_DEVICE := d2spr
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SPH-L710
+PRODUCT_MANUFACTURER := Samsung
