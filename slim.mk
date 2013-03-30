67c5a81689e44984d88761450a30ceddf578e1ee
cordell <cordellsmail@gmail.com>
2013-03-30 14:56:13 +0100

    d2spr: fix description/fingerprint (they were transposed)
    Change-Id: Iee0b628494c041cc7b7db3cb545f17d904bdeeaf

--- slim.mk
+++ slim.mk
@@ -23,7 +23,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES +=  \
     vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2spr TARGET_DEVICE=d2spr BUILD_FINGERPRINT="d2spr-user 4.0.4 IMM76D L710VPALG2 release-keys" PRIVATE_BUILD_DESC="samsung/d2spr/d2spr:4.0.4/IMM76D/L710VPALG2:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2spr TARGET_DEVICE=d2spr BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.0.4/IMM76D/L710VPALG2:user/release-keys" PRIVATE_BUILD_DESC="d2spr-user 4.0.4 IMM76D L710VPALG2 release-keys"
 
 PRODUCT_NAME := slim_d2spr
 PRODUCT_DEVICE := d2spr
