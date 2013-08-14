7123c547266b12e3279d17a15404e01e72d20919
nicklovell23 <nicklovell23@gmail.com>
2013-08-14 05:17:29 +0200

    Adjust slim.mk for new bootanimation
    Change-Id: Ica9a66340c47adcd41d59f0e9392923896606366

--- slim.mk
+++ slim.mk
@@ -15,10 +15,6 @@ $(call inherit-product, vendor/slim/config/common_sgs.mk)
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
-#bootanimation
-PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2spr TARGET_DEVICE=d2spr BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.0.4/IMM76D/L710VPALG2:user/release-keys" PRIVATE_BUILD_DESC="d2spr-user 4.0.4 IMM76D L710VPALG2 release-keys"
 
 PRODUCT_NAME := slim_d2spr
