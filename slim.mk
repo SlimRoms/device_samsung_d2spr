c52378a5e4624b3ff8a0ac5ce3dcc4f12ca95e4a
nicklovell23 <nicklovell23@gmail.com>
2013-12-25 06:27:46 +0100

    Remove common led flash
    Change-Id: I53497a689f4d3139c6867253455ecafdb70858a4

--- slim.mk
+++ slim.mk
@@ -9,9 +9,6 @@ $(call inherit-product, vendor/slim/config/cdma.mk)
 # Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2spr TARGET_DEVICE=d2spr BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.0.4/IMM76D/L710VPALG2:user/release-keys" PRIVATE_BUILD_DESC="d2spr-user 4.0.4 IMM76D L710VPALG2 release-keys"
 
 PRODUCT_NAME := slim_d2spr
