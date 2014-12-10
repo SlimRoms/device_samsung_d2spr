c69a3a1c758ecb607470d71bf89453a4085e65bd
Perseus71 <perseus71@gmail.com>
2014-12-10 13:46:16 +0100

    Correcting device.
    Change-Id: Ib1495319aaaecb4e957d255a1fc053715c1ac6ce

--- slim.mk
+++ slim.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/d2att/full_d2att.mk)
+$(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
